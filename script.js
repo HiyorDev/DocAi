327530f203411b39e9237645e4c55be5065d1555
HiyorDev <mauricio.vega.dev@gmail.com>
2026-09-02 15:26:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // API CONNECTION
 // ===============================
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://docai-0uf4.onrender.com";
 
 //
 async function uploadDocument(file) {
